544dd6cf3d07480342361933987f7c50b581a4ee
bekbola <azizbekbaxadirovich@gmail.com>
2024-11-08 11:38:20 +0500

    lab-9

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Category, Region, New
 from django.urls import reverse_lazy
 
@@ -63,3 +65,22 @@ class CreatePost(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('home')
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'registration/logged_out.html')
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name= 'registration/signup.html'
+
+
+class Edit_post(UpdateView):
+    model= New
+    template_name = 'edit-post.html'
+    fields = ['category', 'region', 'title', 'text']
+class Delete_post(DeleteView):
+    model = New
+    template_name = 'delete-post.html'
+    success_url = reverse_lazy('home')
