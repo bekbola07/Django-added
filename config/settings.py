f25d48538961e6bcd81fb44721c2b9f57ea62db3
bekbola <azizbekbaxadirovich@gmail.com>
2024-11-15 22:25:29 +0500

    requirement added

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,9 @@ MEDIA_ROOT = BASE_DIR
 SECRET_KEY = 'django-insecure-14^c)-dwv^3=y-h1a@%4o=w90-6ci4p_+ti_v%yngt#0hgv6$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
